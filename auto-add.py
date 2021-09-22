9dfafedac1a705a10a451671ebae96431c0bb5a9
shadowninja024 <63149314+shadowninja024@users.noreply.github.com>
2021-09-22 19:29:40 +0400

    Update auto-add.py

--- auto-add.py
+++ auto-add.py
@@ -13,9 +13,9 @@ import random
 import re
 
 
-api_id = 12345    #Your Api ID
-api_hash = 'XXXXXXXXXXXXXXX'    #Your Api Hash
-phone = '+11234567890'      # Your Phone Number With Country Code.
+api_id = 1046736    #Your Api ID
+api_hash = 'e869ac40c2ee5b151cd7b2f55c88a686'    #Your Api Hash
+phone = '+917411338609'      # Your Phone Number With Country Code.
 client = TelegramClient(phone, api_id, api_hash)
 
 client.connect()
